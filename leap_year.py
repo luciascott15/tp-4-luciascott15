75050d89a4334db3b07811b422059a5e725cf9c8
luciascott15 <lscott@mail.austral.edu.ar>
2025-04-25 12:17:48 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,24 @@
+import math
 def leap_year():
-    print("TO DO")
+     A = float(input("Ingrese el coeficiente A:"))
+    B = float(input("Ingrese el coeficiente B:"))
+    X1 = float(input("Ingrese el coeficiente X1:"))
+    X2 = float(input("Ingrese el coeficiente X2"))
+
+    print(f"El coeficiente A de su ecuación de la recta es: {A}")
+    print(f"El coeficiente B de su ecuación de la recta es: {B}")
+    print(f"El coeficiente X1 de su ecuación de la recta es: {X1}")
+    print(f"El coeficiente X2 de su ecuación de la recta es: {X2}")
+    print()
+    print("Para la siguiente ecuacion:")
+    print(f"\tY = {A}X + {B}\n")
+    print()
+    Y1 = A*X1 + B
+    Y2 = A*X2 + B
+
+    print("Dados los siguientes puntos:")
+    print(f"\tP1 ({X1}, {Y1})")
+    print(f"\tP2 ({X2}, {Y2})\n")
+    print()
+    distancia = math.sqrt((X2 - X1)**2 + (Y2 - Y1)**2)
+    print(f"La distancia entre ellos es: {distancia}")
